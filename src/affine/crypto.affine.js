be3f53717ecf4016eaba84b9e034f1009199fcce
Valentin Starck <valentin.starck@gmail.com>
2011-11-15 09:28:20 -0300

    Test & arguments validation

--- src/affine/crypto.affine.js
+++ src/affine/crypto.affine.js
@@ -1,16 +1,27 @@
 var crypto = {
-	alpha: 'abcdefghijklmnopqrstuvwxyz'
+    alpha: 'abcdefghijklmnopqrstuvwxyz',
+    gcd: function(a, b) {
+        if (b) {
+            return crypto.gcd(b, a % b);
+        } else {
+            return Math.abs(a);
+        }
+    }
 };
 
 crypto.Affine = function(opts) {
-	opts = opts || {}; 
-	this.offset = opts.offset || 0;
-	this.alpha = opts.alpha || crypto.alpha;
-	this.modifier = opts.modifier || 1;
-	
-	if(this.offset < 0) {
-		this.offset = (this.offset % this.alpha.length) + this.alpha.length;
-	}
+    opts = opts || {};
+    this.offset = opts.offset || 0;
+    this.alpha = opts.alpha || crypto.alpha;
+    this.modifier = opts.modifier || 1;
+
+    if(crypto.gcd(this.alpha.length, this.modifier) != 1) {
+        throw 'Invalid arguments!';
+    }
+
+    if (this.offset < 0) {
+        this.offset = (this.offset % this.alpha.length) + this.alpha.length;
+    }
 };
 
 crypto.Affine.prototype.translate = function(str, offset, modifier) {
@@ -18,57 +29,51 @@ crypto.Affine.prototype.translate = function(str, offset, modifier) {
 };
 
 crypto.Affine.prototype.encrypt = function(str) {
-	var 
-		offset = this.offset, 
-		modifier = this.modifier,
-		alpha = this.alpha,
-		alphaSize = alpha.length;
+    var
+        offset = this.offset,
+        modifier = this.modifier,
+        alpha = this.alpha,
+        alphaSize = alpha.length;
 
     return str.split('').reduce(function(memo, chr, i) {
         var
-                index = alpha.indexOf(chr.toLowerCase()),
-				// es mayusculas?
-                up = /[A-Z]/.test(chr);
-				
-		// si existe en el alfabeto
+            index = alpha.indexOf(chr.toLowerCase()),
+            // es mayusculas?
+            up = /[A-Z]/.test(chr);
+
+        // si existe en el alfabeto
         if (index != -1) {
-			//debugger;
-			var newIndex = (modifier * index + offset) % alphaSize;
-			// lo traducimos
-			//console.log(chr, ' -> ', index, newIndex);
-            chr = alpha[newIndex ];
+            index = (modifier * index + offset) % alphaSize;
+
+            // lo traducimos
+            chr = alpha[index];
         }
 
         return memo + ( up ? chr.toUpperCase() : chr);
     }, '');
 };
 
-crypto.Affine.prototype.decrypt = function(str) {	
-	var 
-		offset = -this.offset, 
-		modifier = 1/this.modifier,
-		alpha = this.alpha,
-		alphaSize = alpha.length;
+crypto.Affine.prototype.decrypt = function(str) {
+    var
+        offset = -this.offset,
+        modifier = 1 / this.modifier,
+        alpha = this.alpha,
+        alphaSize = alpha.length;
 
     return str.split('').reduce(function(memo, chr, i) {
         var
-                index = alpha.indexOf(chr.toLowerCase()),
-				// es mayusculas?
-                up = /[A-Z]/.test(chr);
-				
-		// si existe en el alfabeto
+            index = alpha.indexOf(chr.toLowerCase()),
+            // es mayusculas?
+            up = /[A-Z]/.test(chr);
+
+        // si existe en el alfabeto
         if (index != -1) {
-			index =+ offset;
-		
-			while(modifier * index != Math.floor(modifier * index)) {
-				index += alphaSize;
-			}
-		
-			var newIndex = (modifier * index) % alphaSize;
-			
-			// lo traducimos
-			//console.log(chr, ' -> ', index, newIndex);
-            chr = alpha[newIndex];
+            index = index + offset;
+
+            while (modifier * index != Math.floor(modifier * index))  index += alphaSize;
+
+            // lo traducimos
+            chr = alpha[modifier * index];
         }
 
         return memo + ( up ? chr.toUpperCase() : chr);
